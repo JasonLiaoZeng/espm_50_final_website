e83b07961a49be6b59f2c14a41c4f6bce8d0bd1a
JasonLiaoZeng <jlz857609223@yahoo.com>
2023-03-07 17:32:31 -0800

    no longer ignre build

---
+++ build/precache-manifest.e5e3b0fd09dee2b0f2e1a603fdfd0ebc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3644fcdf41a57fbb7bd",
+    "url": "/espm_50_final_website/static/css/main.11149fa7.chunk.css"
+  },
+  {
+    "revision": "f3644fcdf41a57fbb7bd",
+    "url": "/espm_50_final_website/static/js/main.f3644fcd.chunk.js"
+  },
+  {
+    "revision": "77c2899f71d0d5323db8",
+    "url": "/espm_50_final_website/static/js/1.77c2899f.chunk.js"
+  },
+  {
+    "revision": "6f944f83053f6360d3b5",
+    "url": "/espm_50_final_website/static/js/runtime~main.6f944f83.js"
+  },
+  {
+    "revision": "b1614afd389ccffadedb4442ca5fe69e",
+    "url": "/espm_50_final_website/static/media/forest_and_road.b1614afd.jpg"
+  },
+  {
+    "revision": "75619ef4dae030f4a657347a1963194a",
+    "url": "/espm_50_final_website/static/media/electric.75619ef4.jpg"
+  },
+  {
+    "revision": "42b7d2ff21067e994c7cdefe5cf5e971",
+    "url": "/espm_50_final_website/index.html"
+  }
+];
